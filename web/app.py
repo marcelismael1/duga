6269fe8a6c0ed2cfefcc5d849a3e06ff4cb09360
Nasulaiman <72337248+Nasulaiman@users.noreply.github.com>
2020-11-12 19:27:32 +0100

    Add static files, charts to Dashboard page

--- web/app.py
+++ web/app.py
@@ -2,15 +2,28 @@ from flask import Flask, render_template, url_for, redirect
 
 app = Flask(__name__)
 
+# Static Data
+headings = ("System IP","DATE", "CVE NO", "SEVERITY", "AFFECTED PACKAGES", "PACKAGE NUMBER")
+
+data = (
+	("210.x.x.x", "2020-Sep-27 18:20", "CVE-210.x.x.x 2020-Sep-27 18:20 2019-1010298", "Critical", "Chrome", "50.23"),
+	("210.x.x.x", "2020-Sep-27 18:20", "CVE-210.x.x.x 2020-Sep-27 18:20 2019-1010298", "Critical", "Chrome", "50.23"),
+	("210.x.x.x", "2020-Sep-27 18:20", "CVE-210.x.x.x 2020-Sep-27 18:20 2019-1010298", "Critical", "Chrome", "50.23"),
+
+	)
+
+SOFAlarms = [300,50,100,200]
+Totalseverity = [600,50,100,200]
+
 # Main route
 @app.route('/')
 def main():
-    return render_template("dashboard.html")
+    return dashboard()
 
 # dashboards endpoint
 @app.route('/dashboard')
 def dashboard():
-    return render_template("dashboard.html")
+    return render_template("dashboard.html", headings=headings, data=data, SOFAlarms=SOFAlarms, Totalseverity=Totalseverity)
 
 # alerts endpoint
 @app.route('/alerts')
